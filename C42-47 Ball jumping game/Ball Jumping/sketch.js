882a9f31d303fa7e1f61e985b4b1a14f3f2a9df8
AbhinavKumar07 <74889782+AbhinavKumar07@users.noreply.github.com>
2021-03-21 19:57:07 -0700

    Add files via upload

---
+++ C42-47 Ball jumping game/Ball Jumping/sketch.js
@@ -0,0 +1,30 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var basketball, basketballImage;
+var fidgetSpiner , fidgetSpinerImage;
+var leftArrow,leftArrowImage;
+var rightArrow,rightArrowImage;
+var star , starImage;
+var table, tableImage;
+
+function preload(){
+basketballImage = loadImage(basketball,"Images/Basketball.PNG");
+fidgetSpinerImage = loadImage(fidgetSpiner,"Images/Fidget_Spinner.PNG");
+leftArrowImage = loadImage(leftArrow,"Images/Left_Arrow.PNG");
+rightArrowImage = loadImage(rightArrow,"Images/Right_Arrow.PNG");
+starImage = loadImage(star,"Images/Star.PNG");
+tableImage = loadImage(table,"Images/Table.PNG")
+}
+
+function setup(){
+
+}
+
+function draw(){
+
+}
